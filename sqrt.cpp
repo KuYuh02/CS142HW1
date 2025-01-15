e749914a8c1514c71f9050eca24f617f91849172
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-01-14 17:08:52 -0800

    new test

--- sqrt.cpp
+++ sqrt.cpp
@@ -2,23 +2,23 @@
 #include <cmath>
 #include <cstdint>
 
-extern "C" void fastsqrt(const float* in, float* out, unsigned count) {
-    for (unsigned i = 0; i < count; ++i) {
-        float x = in[i];
-        float guess = x / 2.0f; // Initial guess
-        const float tolerance = 1e-6f;
-        const unsigned maxIterations = 20;
-        unsigned iterations = 0;
+extern "C" void fastsqrt(const float* in, float* out, unsigned count) 
+{
+    while(int i = 0; i < count; i++)
+    {
+        float x = in[0]
+        float n = 2.0f
+        const float tolerance = 1e-5f * x;
 
-        while (iterations < maxIterations) {
-            float newGuess = 0.5f * (guess + x / guess);
-            if (fabs(newGuess - guess) < tolerance) {
+        for (unsigned interations = 0; iterations < 15; iterations++)
+        {
+            float newGuess = 0.5f * (guess + (x/guess));
+            if (fabs(newGuess - guess) < tolerance)
+            {
                 break;
             }
             guess = newGuess;
-            ++iterations;
         }
-
         out[i] = guess;
     }
 }
