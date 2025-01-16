e3dc6b3067178920225a15037bc3d32e956714c1
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-01-15 20:16:04 -0800

    Update sqrt.cpp
    new implementation

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,34 +1,30 @@
 #include <cstdint>
 #include <cmath>
 
-inline float initial_guess(float x) 
-{
-    union 
-    {
-        float y;
+inline float initial_guess(float x) {
+    union {
+        float f;
         uint32_t i;
-    } 
-    u = {x};
+    } u = {x};
 
-    // Approximate sqrt by manipulating the exponent
+    // Adjust the bit-level representation to estimate the square root
     u.i = (u.i >> 1) + 0x1FC00000;
-    return u.y;
+    return u.f;
 }
 
-extern "C" void fastsqrt(const float* in, float* out, unsigned count) 
-{
-    //Initialize variables
-    for (unsigned i = 0; i < count; i++) 
-    {
-        float a = in[i];
-        float b = initial_guess(a);
+extern "C" void fastsqrt(const float* in, float* out, unsigned count) {
+    unsigned i = 0;
+    while (i < count) {
+        float x = in[i];
+        float y = initial_guess(x);
 
-        // Perform Newton's Method'
-        for (int j = 0; j < 4; j++) 
-        {
-            b = 0.5f * (b + a / b);
-        }
+        int j = 0;
+        do {
+            y = 0.5f * (y + x / y);
+            ++j;
+        } while (j < 4);
 
-        out[i] = b;
+        out[i] = y;
+        ++i;
     }
 }
